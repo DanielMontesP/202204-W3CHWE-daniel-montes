9f7cffccf3700842dab1f016ad84d022dc826981
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-22 21:22:59 +0200

    Fix prettier conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
+    jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
+    "import/extensions": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "no-new": "off",
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
   },
 };
